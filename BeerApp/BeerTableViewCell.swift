1e11282c40407c88e0ba427d28367c0e878fec4f
dennislitjens <dennis.litjens@gmail.com>
2017-10-06 16:43:01 +0200

    Adding constraints for auto layout to home screen and detail page

---
+++ BeerApp/BeerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BeerTableViewCell.swift
+//  BeerApp
+//
+//  Created by Dennis Litjens on 2/10/17.
+//  Copyright © 2017 Dennis Litjens. All rights reserved.
+//
+
+import UIKit
+
+class BeerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BeerApp/RatingControl.swift
@@ -0,0 +1,9 @@
+//
+//  RatingControl.swift
+//  BeerApp
+//
+//  Created by Dennis Litjens on 2/10/17.
+//  Copyright © 2017 Dennis Litjens. All rights reserved.
+//
+
+import Foundation
